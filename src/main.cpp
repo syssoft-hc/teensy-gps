be2a99171bb8ed03ab0d5500f27e264b56e53fd8
Peter Sturm <sturm@uni-trier.de>
2022-04-14 15:35:54 +0200

    Upload succeeded
    ... and Teensy does receive messages from the GPS device again. So it's 115200 baud between host and Teensy and 9600 baud between Teensy and GPS.

--- src/main.cpp
+++ src/main.cpp
@@ -23,41 +23,18 @@ void setup() {
   pinMode(busyPin, OUTPUT);
   
   // Initialize Navilock GPS with default configuration
-  gps.Init(38400);
+  gps.Init(9600);
     
   gps_fixed = false;
 }
 
 void loop() {
-  digitalWrite(busyPin, HIGH); // CPU is busy now
-  unsigned long now = millis();
-  unsigned long secs = now / 1000;
-  Serial.print(secs);
-  Serial.println(": loop() again ...");
-    
+  if (millis() / 1000 % 2 == 0)
+    digitalWrite(busyPin, HIGH);
+  else
+    digitalWrite(busyPin, LOW);;
+
   // Catch any message sent from GPS device as a single string
   char *message = gps.GPSMessage(NULL,false);
-  Serial.print("NMEA Sequence: ");
   Serial.println(message);
-  
-  // Catch any message sent from GPS device as a set of parameters
-  message = gps.GPSMessage(NULL,true);
-  for (int p=0; p<gps.EntryCount(); p++) {
-    Serial.print("Entry ");
-    Serial.print(p);
-    Serial.print(" -> ");
-    Serial.println(gps.MessageEntry(p));
-  }
-
-  // Catch next GPRMC message sent from GPS device as a single string
-  message = gps.GPSMessage((char *) "GPRMC",false);
-  Serial.print("Should be GPRMC Sequence: ");
-  Serial.println(message);
-  
-  Serial.println();
-  
-  digitalWrite(busyPin, LOW);
-  
-  // This example reads gps once a second
-  delay(5000 - millis() % 1000);
 }
